eb15912cb6d7075e1ad6c8a0cf4eec19217c1118
lucas.ducrot <ducrot.lucas@gmail.com>
2024-10-01 10:57:58 +0200

    addition survexlike tutorial

---
+++ Lucas/src/explainer.py
@@ -0,0 +1,137 @@
+import numpy as np
+from scipy.interpolate import interp1d
+
+class explainer():
+    """
+    A class to define the explaination model
+
+    Parameters
+    ----------
+    model
+        A survival model to be explained
+
+    data :  `np.ndarray`, shape=(n_samples, n_features)
+        Covariates of new observations need to be explained
+
+    label :  `np.ndarray`, shape=(n_samples, 2), default = None
+		Survival label of new observations
+
+    time_generation : `str`, default = "quantile"
+        Method used to generate times
+
+    sf :
+        Method to predict survival function
+
+    chf :
+        Method to predict cumulative hazard function
+	"""
+    
+    def __init__(self, model, data, label, times = None,time_generation = "quantile", sf = None, chf = None , categorical_data_col = None , categorical_data_treatment = None ):
+        self.model = model
+		# TODO: Check the availability of data, label
+        self.data = data
+        self.label = label
+
+        self.X = self.data.values
+        
+        self.categorical_data_col = categorical_data_col
+        self.categorical_data_treatment = categorical_data_treatment 
+
+<<<<<<< HEAD
+        if sf is not None:
+            self.sf = sf
+        else:
+            self.sf = model.predict_survival_function
+        
+        if chf is not None:
+            self.chf = chf
+        else:
+            self.chf = model.predict_cumulative_hazard_function
+
+        if times is None:
+            survival_times = label[:, 0]
+=======
+		if sf is not None:
+			self.sf = sf
+		elif "sksurv" in model.__module__:
+			self.sf = model.predict_survival_function
+		elif "pycox" in model.__module__:
+			self.sf = model.predict_surv_df
+		else:
+			raise ValueError("Unsupported model")
+
+		if chf is not None:
+			self.chf = chf
+		elif "sksurv" in model.__module__:
+			self.chf = model.predict_cumulative_hazard_function
+		elif "pycox" in model.__module__:
+			self.chf = model.predict_cumulative_hazards
+		else:
+			raise ValueError("Unsupported model")
+>>>>>>> dev_vantuan
+
+        if time_generation == "quantile":
+            qt_list = np.arange(0.05, 0.95, 0.05)
+            self.times = np.quantile(survival_times, qt_list)
+
+        elif time_generation == "uniform":
+            self.times = np.linspace(min(survival_times), max(survival_times), 50)
+
+        else:
+            self.times = np.unique(survival_times)[::10]
+            
+            
+                
+    def predict_survival_function_pycox(self, newdata):
+        results=[]
+            
+        if len(newdata.shape)==1:
+            surv=self.model.predict_surv_df(np.array([newdata]))
+            t=np.array([surv.index])
+            surv_prob=np.array([surv])
+                     
+            surv_func = interp1d(np.insert(t,0,0), np.insert(surv_prob,0,1), kind='previous', fill_value="extrapolate")
+                
+            results.append(surv_func)
+                     
+            return results
+                 
+        elif len(newdata.shape)==2 and newdata.shape[0]==1:
+            surv=self.model.predict_surv_df(newdata)
+            t=np.array([surv.index])
+            surv_prob=np.array([surv])
+                     
+            surv_func = interp1d(np.insert(t,0,0), np.insert(surv_prob,0,1), kind='previous', fill_value="extrapolate")
+                
+            results.append(surv_func)
+                     
+            return results
+                 
+        else:
+                     
+            surv=self.model.predict_surv_df(newdata)
+            t=np.array([surv.index])
+                     
+            surv_prob=np.array(surv)
+                     
+                     
+            for i in range(0,surv_prob.shape[1],1):
+                surv_prob_ind=surv_prob[:,i]
+                surv_func = interp1d(np.insert(t,0,0), np.insert(surv_prob_ind,0,1), kind='previous', fill_value="extrapolate")
+                         
+                results.append(surv_func)
+                     
+            return results
+
+
+    def predict_cumulative_hazard_function_pycox(self, newdata):
+        results=[]
+        if len(newdata.shape)==1:
+            results.append(-np.log(self.predict_survival_function_pycox(newdata)))
+        elif len(newdata.shape)==2 and newdata.shape[0]==1:
+            results.append(-np.log(self.predict_survival_function_pycox(newdata)))
+        else:
+            for i in range(newdata.shape[0]):
+                results.append(-np.log(self.predict_survival_function_pycox(newdata[i,:])))
+        
+        return results
---
+++ Lucas/src/performance.py
@@ -0,0 +1,114 @@
+import numpy as np
+import pandas as pd
+from sksurv.metrics import concordance_index_censored, brier_score, cumulative_dynamic_auc
+from src.prediction import predict
+from src.utils import convert_surv_label_structarray
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.set(style='whitegrid',font="STIXGeneral",context='talk',palette='colorblind')
+
+def evaluate(explainer, data, label, times=None, metric="brier_score"):
+	"""
+	Calculate survival model performance based on the desired metric
+
+
+	Parameters
+	----------
+	explainer :  `class`
+		A Python class used to explain the survival model
+
+	data :  `np.ndarray`, shape=(n_samples, n_feattures)
+		Data used for the prediction
+
+	times :  `np.ndarray`, shape=(n_times,), default = None
+		An array of times for the desired prediction to be evaluated at
+
+	metric : `str`, default = "brier_score"
+		The character of metric, either "brier_score", "c_index" or "auc" depending
+		on the desired metric
+
+
+	Returns
+	-------
+	res : `np.ndarray`, shape=(n_times, )
+		The matrix contains the evaluation of the desired metric
+	"""
+	if times is None:
+		times = explainer.times
+	else:
+		times = np.unique(times)
+	n_times = len(times)
+	if isinstance(data, pd.DataFrame):
+		feats = data.values
+	else:
+		feats = data
+	surv_pred = predict(explainer, feats, times)
+	survival_time =label[:, 0]
+	survival_indicator = label[:, 1].astype(bool)
+	res = np.zeros(n_times)
+	if metric == "c_index":
+		for j in range(n_times):
+			res[j] = concordance_index_censored(survival_indicator, survival_time, surv_pred[:, j])[0]
+
+	elif metric == "brier_score":
+		label_st = convert_surv_label_structarray(label)
+		res = brier_score(label_st, label_st, surv_pred, times)[1]
+
+	elif metric == "auc":
+		label_st = convert_surv_label_structarray(label)
+		for j in range(n_times):
+			res[j] = cumulative_dynamic_auc(label_st, label_st, surv_pred[:, j], times[j])[0]
+
+	res = pd.DataFrame(data=np.stack([times, res]).T, columns=["times", "perf"])
+
+	return res
+
+def plot_performance(perf, metric):
+	"""
+	Plot the prediction of survival model
+
+
+	Parameters
+	----------
+
+	perf :  `np.ndarray`, shape=(n_times,)
+		An array of evaluation values for the desired type of metric
+
+	metric : `str`, default = "brier_score"
+		The character of metric, either "brier_score", "c_index" or "auc" depending
+		on the desired metric
+
+	"""
+	_, ax = plt.subplots(figsize=(9, 5))
+	[x.set_linewidth(2) for x in ax.spines.values()]
+	[x.set_edgecolor('black') for x in ax.spines.values()]
+	sns.lineplot(data=perf, x="times", y="perf")
+	ax.set_xlim(min(perf.times.values), max(perf.times.values))
+	plt.xlabel("Times", fontsize=20)
+	if metric == "c_index":
+		ax.set_ylim(0, 1)
+		plt.ylabel("C-Index", fontsize=20)
+	elif metric == "brier_score":
+		ax.set_ylim(0, .5)
+		plt.ylabel("Brier score", fontsize=20)
+	elif metric == "auc":
+		ax.set_ylim(0, 1)
+		plt.ylabel("AUC", fontsize=20)
+	else:
+		raise ValueError("Only support output type c_index, brier_score, auc")
+
+	plt.show()
+
+def Brier_Score():
+
+	return None
+
+
+def C_index():
+
+	return None
+
+
+def AUC():
+
+	return None
